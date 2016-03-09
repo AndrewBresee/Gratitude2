91b7b429c3080372f6adba7f4cab2dde82151e99
Andrew Bresee <andrew.p.bresee@gmail.com>
2016-03-08 16:12:07 -0800

    Remove node_modules and add MongoDB connection

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 require('./router')(app, express);
+
+mongoose.connect('mongodb://localhost/flock');
+var db = mongoose.connection;
+
+db.on('error', function() {
+  console.log('Error connecting to the database');
+});
+
+db.once('open', function() {
+  console.log('MongoDB is open');
+});
 
 var port = process.env.PORT || 8080;
 
