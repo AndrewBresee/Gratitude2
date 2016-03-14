956c4377e9b60affe8c4564140c0cc7c6de86220
Andrew Bresee <andrew.p.bresee@gmail.com>
2016-03-13 17:43:05 -0700

    Implement CRUD funcitonality

--- router.js
+++ router.js
@@ -16,7 +16,8 @@ var jsonParser = bodyParser.json();
 
 module.exports = function(app, express) {
 
-  app.post('/', jsonParser, function (req, res, next) {
+  //Will create a post with a title and content
+  app.post('/api/create', jsonParser, function (req, res, next) {
     //This console.log tells me the post request in coming in right from postman
     console.log("REQUEST FROM POST :" , req.body);
     var post = new Post({
@@ -32,7 +33,8 @@ module.exports = function(app, express) {
     });
   });
 
-  app.get('/', jsonParser, function(req, res){
+  //Will get a specific post based on a requested title
+  app.get('/api/read', jsonParser, function(req, res){
     console.log("GET REQUEST GOT");
     console.log("req.body :", req.headers.title);
     var requestedTitle = req.headers.title;
@@ -47,21 +49,24 @@ module.exports = function(app, express) {
     });
   });
 
-  app.post('/update', jsonParser, function (req, res, next) {
+  //What does next(err) do?
+  //It seems that including {upsert: true} makes it so that if there isn't anything with that post, it will make a new one.
+  app.post('/api/update', jsonParser, function (req, res, next) {
     //This console.log tells me the post request in coming in right from postman
     console.log("UPDATE POST :" , req.body);
     var updateTitle = req.body.title;
     var updateContent = req.body.content;
     Post.update({ title: updateTitle}, { $set: { content: updateContent }}, function(err, data){
       if (err) {
-        return next(err);
+        res.statusCode(404).json(err);
+        //return next(err);
       } else {
-        res.json(201);
+        res.json(200, data);
       }
     });
   });
 
-  app.get('/delete', function(req, res){
+  app.delete('/api/delete', function(req, res){
     var requestedTitle = req.headers.title;
     Post.find({title: requestedTitle}).remove(function(err, data){
       if(err){
@@ -69,13 +74,10 @@ module.exports = function(app, express) {
         console.log("Could not find");
       } else {
         res.send(200, data);
-        console.log("Data Found!");
+        console.log("Data Deleted!");
       }
     });
   });
-
-
-  //For updating, Post.update({ title: title}, { $set: { content: something }}, callback);
 
 
 };
