369b98a353e5ab549d5dd1418ba8d0268b2cb207
Andrew Bresee <andrew.p.bresee@gmail.com>
2016-03-13 17:47:11 -0700

    Refactor update to be PUT instead of POST. Add comments and questions

--- router.js
+++ router.js
@@ -17,7 +17,7 @@ var jsonParser = bodyParser.json();
 module.exports = function(app, express) {
 
   //Will create a post with a title and content
-  app.post('/api/create', jsonParser, function (req, res, next) {
+  app.post('/', jsonParser, function (req, res, next) {
     //This console.log tells me the post request in coming in right from postman
     console.log("REQUEST FROM POST :" , req.body);
     var post = new Post({
@@ -34,7 +34,8 @@ module.exports = function(app, express) {
   });
 
   //Will get a specific post based on a requested title
-  app.get('/api/read', jsonParser, function(req, res){
+  //Somewhat confused about using headers when doing GET
+  app.get('/', jsonParser, function(req, res){
     console.log("GET REQUEST GOT");
     console.log("req.body :", req.headers.title);
     var requestedTitle = req.headers.title;
@@ -51,7 +52,7 @@ module.exports = function(app, express) {
 
   //What does next(err) do?
   //It seems that including {upsert: true} makes it so that if there isn't anything with that post, it will make a new one.
-  app.post('/api/update', jsonParser, function (req, res, next) {
+  app.put('/', jsonParser, function (req, res, next) {
     //This console.log tells me the post request in coming in right from postman
     console.log("UPDATE POST :" , req.body);
     var updateTitle = req.body.title;
@@ -66,7 +67,7 @@ module.exports = function(app, express) {
     });
   });
 
-  app.delete('/api/delete', function(req, res){
+  app.delete('/', function(req, res){
     var requestedTitle = req.headers.title;
     Post.find({title: requestedTitle}).remove(function(err, data){
       if(err){
