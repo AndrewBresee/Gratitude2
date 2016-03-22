4cfe44fff6419618f2be522e93b24aaee36e34e3
Andrew Bresee <andrew.p.bresee@gmail.com>
2016-03-21 21:21:19 -0700

    Make a userController file and export functionality from router

--- router.js
+++ router.js
@@ -8,6 +8,8 @@ var bcrypt = require('bcrypt');
 var Post = require('./post.js');
 var User = require('./user.js');
 
+var userController = require('./userController');
+
 // allows the post method to use bodyParser.
 var jsonParser = bodyParser.json();
 
@@ -15,55 +17,18 @@ module.exports = function(app, express) {
 
 //***USER INFORMATION**//
 
-  //?? What is the difference between a url path and the api path?
-    //The url path is where a user will actually go,
-    //the api path is a route that is taken internally?
-    //When we go to a speciic file path in postman, is that the same as going to the path on the browser?
-
+//?? What is the difference between a url path and the api path?
+  //The url path is where a user will actually go,
+  //the api path is a route that is taken internally?
+  //When we go to a speciic file path in postman, is that the same as going to the path on the browser?
 
 //TODO: Get bcyrpt to hash passwords
   //Setup routing for different paths
   //Make frontend
   //Check if user exists
-  app.post('/signup', jsonParser, function (req, res, next) {
-    var user = new User({
-      username: req.body.userName,
-      password: req.body.password
-    });
-    user.save(function (err, data) {
-      if (err) {
-        return next(err);
-      } else {
-        //This line saves the user on the session
-        //Without hash, it will save their password directly.
-        //Maybe should only save their username
-        //TODO: only save user id to session
-          req.session.user = data;
-        // res.redirect('/dashboard'); --> will use this later when pages are visable.
-        res.json(201, user);
-      }
-    });
-  });
-
-  app.post('/login', jsonParser, function (req, res, next) {
-    var username = req.body.userName;
-    var password = req.body.password;
-    User.findOne({username: username , password: password}, function(err, user){
-      if(err){
-        res.statusCode(404).json(err);
-        console.log("User Does Not Exist");
-      } else {
-        req.session.user = user;
-        res.json(200, user);
-      }
-    });
-  });
-
-  app.get('/logout', function(req, res){
-    req.session.destroy();
-    res.json("You have logged out");
-    console.log("SESSION GONE! SESSION IS NOW : ", req.session);
-  });
+  app.post('/signup', jsonParser, userController.signUp);
+  app.post('/login', jsonParser, userController.login);
+  app.get('/logout', userController.logout);
 
 //***ROUTING INFORMATION**//
 app.get('/postPage', function(req, res){
@@ -73,6 +38,9 @@ app.get('/postPage', function(req, res){
   }
 });
 
+app.get('/', function(req, res){
+  res.send("This is the home page");
+});
 
 //***POST INFORMATION**//
 
@@ -83,7 +51,7 @@ app.get('/postPage', function(req, res){
   //Will create a post with a title and content
   //On post, it seems we can use req.query instead of having to use middleware jsonParser.
 
-  //Will have to refector to include users information
+  //Will have to refactor to include users information
   app.post('/post', jsonParser, function (req, res, next) {
     if(req.session.user === undefined){
       console.log("You are not logged in!");
@@ -91,11 +59,13 @@ app.get('/postPage', function(req, res){
       //probably need to stop them from continuing here.
       res.json(404);
     }
+
     var post = new Post({
       title: req.body.title,
       content: req.body.content,
       userId: req.session.user._id
     });
+
     post.save(function (err, post) {
       if (err) {
         return next(err);
@@ -106,6 +76,7 @@ app.get('/postPage', function(req, res){
   });
 
   //Will get a specific post based on a requested title
+
   //TODO: Refactor to search for posts based on users information
     //Display all posts that the loged in user generated.
   app.get('/getUser/:title', function(req, res){
