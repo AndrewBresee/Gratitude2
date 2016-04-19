5dbe7c30141e460634b134322a45c5d82f1f8660
Andrew Bresee <andrew.p.bresee@gmail.com>
2016-04-18 22:36:44 -0700

    Add views for signing in and signing up

---
+++ Client/Post/auth.js
@@ -0,0 +1,11 @@
+angular.module('gratitute.auth', [])
+
+.controller('AuthController', function ($scope) {
+  $scope.user = {};
+
+  $scope.signin = function () {
+  };
+
+  $scope.signup = function () {
+  };
+});
