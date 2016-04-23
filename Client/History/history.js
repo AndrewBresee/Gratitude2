92df7ec8f32296a8ae34aaee1da6afc6f1361f54
Andrew Bresee <andrew.p.bresee@gmail.com>
2016-04-22 17:33:59 -0700

    Simply code for future use

--- Client/History/history.js
+++ Client/History/history.js
@@ -1,17 +1,7 @@
 angular.module('gratitude.history', [])
 
-.controller('HistoryController', function ($scope, Post, Auth, $location, $window) {
+.controller('HistoryController', function ($scope) {
   $scope.data = {};
-
-  // This will set userName to be what the username is set to on log in. 
-  
-  // $scope.userName = $window.localStorage.getItem('userName');
-
-  // This will check to see if a user is logged in. 
-  // if (!Auth.isAuth()) {
-  //   $location.path('/signin');
-  // }
-
 
   $scope.getAllPosts = function () {
     Post.getAll($scope.userName)
